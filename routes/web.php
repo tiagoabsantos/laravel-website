5997ae6601d4ad9bf2fea51705b16fe2191f3f31
Tiago <tiagosantos.gpsi@gmail.com>
2021-05-17 18:11:00 +0100

    Added Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::get('/new', function () {
+    return view('new');
+})->middleware(['auth'])->name('new');
+
+Route::get('/update', function () {
+    return view('update');
+})->middleware(['auth'])->name('update');
+
+require __DIR__.'/auth.php';
